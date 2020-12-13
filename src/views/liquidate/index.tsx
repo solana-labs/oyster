c1ca4b26a5b262cd335f552d43f6bbc0525ae3c6
Sebastian.Bor <sebastian_bor@hotmail.com>
2020-12-13 14:28:21 +0000

    chore: prettier

--- src/views/liquidate/index.tsx
+++ src/views/liquidate/index.tsx
@@ -9,23 +9,22 @@ export const LiquidateView = () => {
 
   return (
     <div className="liquidate-container">
-      {liquidableObligations.length === 0 && (
+      {liquidableObligations.length === 0 ? (
         <div className="liquidate-info">{LABELS.LIQUIDATE_NO_LOANS}</div>
-      )}
-      {liquidableObligations.length > 0 && (
-        <div className="flexColumn">
-          <div className="liquidate-item liquidate-header">
-            <div>{LABELS.TABLE_TITLE_ASSET}</div>
-            <div>{LABELS.TABLE_TITLE_LOAN_BALANCE}</div>
-            <div>{LABELS.TABLE_TITLE_APY}</div>
-            <div>{LABELS.TABLE_TITLE_LTV}</div>
-            <div>{LABELS.TABLE_TITLE_ACTION}</div>
+      ) : (
+          <div className="flexColumn">
+            <div className="liquidate-item liquidate-header">
+              <div>{LABELS.TABLE_TITLE_ASSET}</div>
+              <div>{LABELS.TABLE_TITLE_LOAN_BALANCE}</div>
+              <div>{LABELS.TABLE_TITLE_APY}</div>
+              <div>{LABELS.TABLE_TITLE_LTV}</div>
+              <div>{LABELS.TABLE_TITLE_ACTION}</div>
+            </div>
+            {liquidableObligations.map((item) => (
+              <LiquidateItem key={item.obligation.pubkey.toBase58()} obligation={item.obligation} ltv={item.ltv}></LiquidateItem>
+            ))}
           </div>
-          {liquidableObligations.map((item) => (
-            <LiquidateItem key={item.obligation.pubkey.toBase58()} obligation={item.obligation} ltv={item.ltv}></LiquidateItem>
-          ))}
-        </div>
-      )}
+        )}
     </div>
   );
 };
