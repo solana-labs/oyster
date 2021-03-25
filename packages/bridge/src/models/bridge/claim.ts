8fc5e1543256b3a2e8553a1e1994b2a82c0a6dac
bartosz-lipinski <264380+bartosz-lipinski@users.noreply.github.com>
2021-03-25 12:22:45 -0500

    feat: add claimed vaa format

--- packages/bridge/src/models/bridge/claim.ts
+++ packages/bridge/src/models/bridge/claim.ts
@@ -1,2 +1,18 @@
 // 40 - ExecutedVAA (claim)
 export const NOP = 0;
+
+/*
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct ClaimedVAA {
+    /// hash of the vaa
+    pub hash: [u8; 32],
+    /// time the vaa was submitted
+    pub vaa_time: u32,
+
+    /// Is `true` if this structure has been initialized.
+    pub is_initialized: bool,
+}
+
+*/
