769c6cf3e6210e2864d4108971d88216dca5baa9
Jordan Prince <jordanmprince@gmail.com>
2021-04-25 22:21:34 -0500

    Some fixes

--- packages/metavinci/src/views/artworks/index.tsx
+++ packages/metavinci/src/views/artworks/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArtCard } from '../../components/ArtCard';
 import { Row, Col } from 'antd';
-import Masonry from 'react-masonry-css'
+import Masonry from 'react-masonry-css';
 import { Link } from 'react-router-dom';
 import { useUserArts } from '../../hooks';
 
@@ -11,13 +11,22 @@ export const ArtworksView = () => {
     default: 4,
     1100: 3,
     700: 2,
-    500: 1
+    500: 1,
   };
 
   return (
     <div className="flexColumn" style={{ flex: 1 }}>
       <Col>
-        <Row style={{ marginBottom: 30, marginTop: 20, fontSize: 20, fontWeight: 600 }}>Owned</Row>
+        <Row
+          style={{
+            marginBottom: 30,
+            marginTop: 20,
+            fontSize: 20,
+            fontWeight: 600,
+          }}
+        >
+          Owned
+        </Row>
         <Row>
           <Masonry
             breakpointCols={breakpointColumnsObj}
@@ -25,15 +34,19 @@ export const ArtworksView = () => {
             columnClassName="my-masonry-grid_column"
           >
             {ownedMetadata.map(m => {
-              const id = m.pubkey.toBase58();
-              return <Link to={`/art/${id}`}>
-                <ArtCard key={id}
-                  image={m.info.extended?.image}
-                  category={m.info.extended?.category}
-                  name={m.info?.name}
-                  symbol={m.info.symbol}
-                  preview={false} />
+              const id = m.metadata.pubkey.toBase58();
+              return (
+                <Link to={`/art/${id}`}>
+                  <ArtCard
+                    key={id}
+                    image={m.metadata.info.extended?.image}
+                    category={m.metadata.info.extended?.category}
+                    name={m.metadata.info?.name}
+                    symbol={m.metadata.info.symbol}
+                    preview={false}
+                  />
                 </Link>
+              );
             })}
           </Masonry>
         </Row>
