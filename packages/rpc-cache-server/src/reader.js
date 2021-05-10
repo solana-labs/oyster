3f3e0a34a06f8050c3c07696700f7d4049732aa0
juan <juandgoc@gmail.com>
2021-05-09 19:13:32 -0500

    Added solana library code

--- packages/rpc-cache-server/src/reader.js
+++ packages/rpc-cache-server/src/reader.js
@@ -1,80 +1,58 @@
-const { JSONRPCServer } = require("json-rpc-2.0");
+const {JSONRPCServer} = require("json-rpc-2.0");
 const express = require("express");
 const bodyParser = require("body-parser");
 const redis = require("redis");
-const crypto = require("crypto");
+const cors = require("cors");
 const axios = require("axios");
 const settings = require("./config");
 
 const server = new JSONRPCServer();
 const redisClient = redis.createClient();
 
-const programIDs = settings.programIDs;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const getProgramAccounts = function({ programID })
-{
-    return new Promise((resolve, reject) => {
-        redisClient.get(programID, function(err, reply) {
-            if (err) {
-                reject(err);
-            } else {
-                if (reply === null) { // if cache miss
-                    // What it needs to do:
-                    // 1. Notify the writer
-                    axios({
-                        method : "post",
-                        url : "http://localhost:3001/",
-                        data : {identifier : programID}
-                    });
-
-                    // 2. Send an RPC request to the full node on behalf of the client
-                    const client = new JSONRPCClient((jsonRPCRequest) =>
-                        axios({
-                            method: "post",
-                            url: "https://solana-api.projectserum.com/", // mainnet url
-                            headers: {
-                                "content-type": "application/json",
-                            },
-                            data: jsonRPCRequest,
-                        }).then((response) => {
-                            if (response.status === 200) {
-                                // Use client.receive when you received a JSON-RPC response.
-                                return client.receive(response.data);
-                            } else if (jsonRPCRequest.id !== undefined) {
-                                return Promise.reject(new Error(response.statusText));
-                            }
-                        })
-                    );
-                    var val = null;
-                    client
-                        .request("getProgramAccounts", { programID: programID })
-                        .then((result) => val = result);
-                    resolve(val);
-                    return;
-                }
-                resolve(reply);
-            }
-        });
+const getProgramAccounts = function (params) {
+  return new Promise((resolve, reject) => {
+    const programID = params[0];
+    const filters = params[1];
+    redisClient.hvals(programID, function (err, reply) {
+      if (err) {
+        reject(err);
+      } else {
+        if (reply === null) { // if cache miss
+          // What it needs to do:
+          // 1. Notify the writer
+          axios({
+            method: "post",
+            url: "http://localhost:3001/",
+            data: {programID, filters}
+          });
+          reject(err)
+        }
+        console.log(JSON.parse(reply))
+        resolve(reply);
+      }
     });
+  });
 };
 server.addMethod("getProgramAccounts", getProgramAccounts);
 
-const app = express();
-app.use(bodyParser.json());
 
 app.post("/json-rpc", (req, res) => {
-    const jsonRPCRequest = req.body;
-    // server.receive takes a JSON-RPC request and returns a Promise of a JSON-RPC response.
-    console.log("received request");
-    console.log(jsonRPCRequest);
-    server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
-        if (jsonRPCResponse) {
-            res.json(jsonRPCResponse);
-        } else {
-            console.log("no response");
-            res.sendStatus(204);
-        }
-    });
+  const jsonRPCRequest = req.body;
+  // server.receive takes a JSON-RPC request and returns a Promise of a JSON-RPC response.
+  // console.log("received request");
+  // console.log(jsonRPCRequest);
+  server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+    if (jsonRPCResponse) {
+      res.json(jsonRPCResponse);
+    } else {
+      console.log("no response");
+      res.sendStatus(204);
+    }
+  });
 });
 
 app.listen(3001);
--- packages/rpc-cache-server/src/test-client.js
+++ packages/rpc-cache-server/src/test-client.js
@@ -1,27 +1,27 @@
-const { JSONRPCClient } = require("json-rpc-2.0");
+const {JSONRPCClient} = require("json-rpc-2.0");
 const axios = require("axios");
 
 // JSONRPCClient needs to know how to send a JSON-RPC request.
 // Tell it by passing a function to its constructor. The function must take a JSON-RPC request and send it.
 const client = new JSONRPCClient((jsonRPCRequest) =>
-  axios({
-      method: "post",
-      url: "http://localhost:3000/json-rpc",
-      headers: {
-          "content-type": "application/json",
-      },
-      data: jsonRPCRequest,
-  }).then((response) => {
-    if (response.status === 200) {
-        // Use client.receive when you received a JSON-RPC response.
-        return client.receive(response.data);
-    } else if (jsonRPCRequest.id !== undefined) {
-      return Promise.reject(new Error(response.statusText));
-    }
-  })
+    axios({
+        method: "post",
+        url: "http://localhost:3000/json-rpc",
+        headers: {
+            "content-type": "application/json",
+        },
+        data: jsonRPCRequest,
+    }).then((response) => {
+        if (response.status === 200) {
+            // Use client.receive when you received a JSON-RPC response.
+            return client.receive(response.data);
+        } else if (jsonRPCRequest.id !== undefined) {
+            return Promise.reject(new Error(response.statusText));
+        }
+    })
 );
 
 client
-    .request("getProgramAccounts", { programID: "WormT3McKhFJ2RkiGpdw9GKvNCrB2aB54gb2uV9MfQC" })
+    .request("getProgramAccounts", {programID: "WormT3McKhFJ2RkiGpdw9GKvNCrB2aB54gb2uV9MfQC"})
     .then((result) => console.log(result));
 
