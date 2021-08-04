e05ecbc9a213ddd087478064e51910119b05b0e4
Juan Diego García <juandgoc@gmail.com>
2021-08-04 10:03:56 -0500

    Added renbtc debug page (#172)
    * fixed button with no provider
    
    * Added renbtc debug page

--- packages/bridge/src/views/index.tsx
+++ packages/bridge/src/views/index.tsx
@@ -4,3 +4,4 @@ export { TransferView } from './transfer';
 export { HelpView } from './help';
 export { FaqView } from './faq';
 export { ProofOfAssetsView } from './proof-of-assets';
+export { RenbtcDebugView } from './renbtc-debug';
