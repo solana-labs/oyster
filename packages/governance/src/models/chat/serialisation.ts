e52d6acc61b9d26df324d4385461d1834a0b2fb5
Sebastian.Bor <sebastian_bor@hotmail.com>
2021-08-25 19:53:29 +0200

    feat: add chat accounts

---
+++ packages/governance/src/models/chat/serialisation.ts
@@ -0,0 +1,36 @@
+import {
+  ChatMessage,
+  GovernanceChatAccountClass,
+  MessageBody,
+} from './accounts';
+import { BorshAccountParser } from '../core/serialisation';
+
+export const GOVERNANCE_CHAT_SCHEMA = new Map<any, any>([
+  [
+    MessageBody,
+    {
+      kind: 'struct',
+      fields: [
+        ['type', 'u8'],
+        ['value', 'string'],
+      ],
+    },
+  ],
+  [
+    ChatMessage,
+    {
+      kind: 'struct',
+      fields: [
+        ['accountType', 'u8'],
+        ['proposal', 'pubkey'],
+        ['author', 'pubkey'],
+        ['postedAt', 'u64'],
+        ['replyTo', { kind: 'option', type: 'pubkey' }],
+        ['body', MessageBody],
+      ],
+    },
+  ],
+]);
+
+export const ChatAccountParser = (classType: GovernanceChatAccountClass) =>
+  BorshAccountParser(classType, GOVERNANCE_CHAT_SCHEMA);
