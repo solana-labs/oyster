b1e4466a7896b5b1dcbb57ca3fe53b7561a23e6b
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-01-11 02:17:09 +0000

    chore: remove dependency on wallet adapter

--- packages/governance-sdk/src/tools/walletAdapter.ts
+++ packages/governance-sdk/src/tools/walletAdapter.ts
@@ -1,6 +1,16 @@
-import { SignerWalletAdapter } from '@solana/wallet-adapter-base';
+import { PublicKey, Transaction } from '@solana/web3.js';
 
-export type WalletSigner = Pick<
-  SignerWalletAdapter,
-  'publicKey' | 'signTransaction' | 'signAllTransactions'
->;
+export type WalletSigner = {
+  publicKey: PublicKey | null;
+  signTransaction(transaction: Transaction): Promise<Transaction>;
+  signAllTransactions(transaction: Transaction[]): Promise<Transaction[]>;
+};
+
+export declare class WalletError extends Error {
+  error: any;
+  constructor(message?: string, error?: any);
+}
+
+export declare class WalletNotConnectedError extends WalletError {
+  name: string;
+}
