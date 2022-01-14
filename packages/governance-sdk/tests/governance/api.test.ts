cb4ed50eb6012e61433fae1455c2e8909fca127c
Sebastian Bor <Sebastian_Bor@hotmail.com>
2022-01-14 11:52:24 +0000

    feat: add getTokenOwnerRecordsByOwner

---
+++ packages/governance-sdk/tests/governance/api.test.ts
@@ -0,0 +1,21 @@
+
+
+import { PublicKey } from '@solana/web3.js';
+//import { getTokenOwnerRecordsByOwner } from '../../src'
+
+
+const programId = new PublicKey("GTesTBiEWE32WHXXE2S4XbZvA5CrEc4xs6ZgRe895dP")
+const rpcEndpoint = "https://psytrbhymqlkfrhudd.dev.genesysgo.net:8899"
+
+
+test('getTokenOwnerRecordsByOwner', async () => {
+  // Arrange
+  const tokenOwnerPk = new PublicKey("EZLvwGdGyeks3jQLWeBbjL1uGeGbqb2MYU4157pDP9ch")
+
+  // Act
+  //const results = await getTokenOwnerRecordsByOwner(rpcEndpoint, programId, tokenOwnerPk)
+
+  // Assert
+
+
+});
