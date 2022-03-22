c8c3e4f325f9f06ae1e851b858ec32d642f1d81e
Sebastian.Bor <sebastian_bor@hotmail.com>
2022-03-22 18:53:06 +0000

    feat: add VoterWeightRecord account and api

--- packages/governance-sdk/src/addins/accounts.ts
+++ packages/governance-sdk/src/addins/accounts.ts
@@ -1,7 +1,76 @@
 import { PublicKey } from '@solana/web3.js';
 import BN from 'bn.js';
 
-export type GovernanceAddinAccountClass = typeof MaxVoterWeightRecord;
+export type GovernanceAddinAccountClass =
+  | typeof VoterWeightRecord
+  | MaxVoterWeightRecord;
+
+export enum VoterWeightAction {
+  CastVote,
+  CommentProposal,
+  CreateGovernance,
+  CreateProposal,
+  SignOffProposal,
+}
+
+export class VoterWeightRecord {
+  accountDiscriminator = new Uint8Array([50, 101, 102, 57, 57, 98, 52, 98]);
+
+  realm: PublicKey;
+
+  governingTokenMint: PublicKey;
+
+  governingTokenOwner: PublicKey;
+
+  voterWeight: BN;
+
+  voterWeightExpiry: BN;
+
+  weightAction: VoterWeightAction | undefined;
+
+  weightActionTarget: PublicKey | undefined;
+
+  constructor(args: {
+    realm: PublicKey;
+    governingTokenMint: PublicKey;
+    governingTokenOwner: PublicKey;
+    voterWeight: BN;
+    voterWeightExpiry: BN;
+    weightAction: VoterWeightAction | undefined;
+    weightActionTarget: PublicKey | undefined;
+  }) {
+    this.realm = args.realm;
+    this.governingTokenMint = args.governingTokenMint;
+    this.governingTokenOwner = args.governingTokenOwner;
+    this.voterWeight = args.voterWeight;
+    this.voterWeightExpiry = args.voterWeightExpiry;
+    this.weightAction = args.weightAction;
+    this.weightActionTarget = args.weightActionTarget;
+  }
+}
+
+/**
+ * Returns the default address for VoterWeightRecord
+ * Note: individual addins are not required to use the default address and it can vary between different implementations
+ **/
+export async function getVoterWeightRecordAddress(
+  programId: PublicKey,
+  realm: PublicKey,
+  governingTokenMint: PublicKey,
+  governingTokenOwner: PublicKey,
+) {
+  const [voterWeightRecordAddress] = await PublicKey.findProgramAddress(
+    [
+      Buffer.from('voter-weight-record'),
+      realm.toBuffer(),
+      governingTokenMint.toBuffer(),
+      governingTokenOwner.toBuffer(),
+    ],
+    programId,
+  );
+
+  return voterWeightRecordAddress;
+}
 
 export class MaxVoterWeightRecord {
   accountDiscriminator = new Uint8Array([57, 100, 53, 102, 102, 50, 57, 55]);
