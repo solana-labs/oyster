0c6c3d2e95b032378442bc0b81ac814042777dba
Wiktor Serow <vikseriq@gmail.com>
2022-06-17 13:11:52 +0200

    fix: update WithdrawGoverningTokens instructions

--- packages/governance-sdk/src/governance/withWithdrawGoverningTokens.ts
+++ packages/governance-sdk/src/governance/withWithdrawGoverningTokens.ts
@@ -4,6 +4,7 @@ import { serialize } from 'borsh';
 import { WithdrawGoverningTokensArgs } from './instructions';
 import { GOVERNANCE_PROGRAM_SEED } from './accounts';
 import { TOKEN_PROGRAM_ID } from '../tools/sdk/splToken';
+import { shortMeta } from '../tools';
 
 export const withWithdrawGoverningTokens = async (
   instructions: TransactionInstruction[],
@@ -35,34 +36,25 @@ export const withWithdrawGoverningTokens = async (
     programId,
   );
 
+  // According to schema https://github.com/neonlabsorg/neon-spl-governance/blob/main/addin-vesting/program/src/instruction.rs#L47-L62
   const keys = [
-    { pubkey: realm, isWritable: false, isSigner: false },
-    {
-      pubkey: governingTokenHoldingAddress,
-      isWritable: true,
-      isSigner: false,
-    },
-    {
-      pubkey: governingTokenDestination,
-      isWritable: true,
-      isSigner: false,
-    },
-    {
-      pubkey: governingTokenOwner,
-      isWritable: false,
-      isSigner: true,
-    },
-    {
-      pubkey: tokenOwnerRecordAddress,
-      isWritable: true,
-      isSigner: false,
-    },
-
-    {
-      pubkey: TOKEN_PROGRAM_ID,
-      isWritable: false,
-      isSigner: false,
-    },
+    // 0. `[]` The spl-token program account
+    shortMeta(TOKEN_PROGRAM_ID),
+    // 1. `[writable]` The vesting account. PDA seeds: [vesting spl-token account]
+    shortMeta(governingTokenDestination, true),
+    // 2. `[writable]` The vesting spl-token account
+    shortMeta(governingTokenHoldingAddress, true),
+    // 3. `[writable]` The destination spl-token account
+    shortMeta(tokenOwnerRecordAddress, true),
+    // 4. `[signer]` The Vesting Owner account
+    shortMeta(governingTokenOwner, false, true),
+    // 5. `[]` The Governance program account
+    shortMeta(TOKEN_PROGRAM_ID),
+    // 6. `[]` The Realm account
+    shortMeta(realm),
+    // 7. `[]` Governing Owner Record. PDA seeds (governance program): ['governance', realm, token_mint, vesting_owner]
+    // 8. `[writable]` The VoterWeightRecord. PDA seeds: ['voter_weight', realm, token_mint, vesting_owner]
+    // 9. `[writable]` The MaxVoterWeightRecord. PDA seeds: ['max_voter_weight', realm, token_mint]
   ];
 
   instructions.push(
