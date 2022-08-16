de4c1bd7e27d5bd10ea38f3c8bbdfa46097829c9
Wiktor Serow <vikseriq@gmail.com>
2022-08-16 12:20:56 +0200

    feat: NDEV-507 add input button for deposit modal fix: correct mint values human-readable formatter

---
+++ packages/governance/src/hooks/useMintFormatter.ts
@@ -0,0 +1,39 @@
+import { PublicKey } from '@solana/web3.js';
+import { BigNumber } from 'bignumber.js';
+import { useMint } from '@oyster/common';
+import BN from 'bn.js';
+
+interface IMintFormatter {
+  closestNumber: (value: string | number | BN) => BN;
+  formatValue: (value: string | BN, asRaw?: boolean) => string;
+  parseValue: (value: string) => BN;
+}
+
+const formatLamports = (value: string | number | BN | BigNumber, decimals: number) => {
+  let v = value;
+  if (v instanceof BN) {
+    v = v.toString();
+  }
+  return new BigNumber(v).shiftedBy(-decimals);
+};
+
+export const useMintFormatter = (mint: PublicKey | undefined) => {
+  const mintInfo = useMint(mint);
+  const decimals = mintInfo?.decimals || 9;
+
+  return {
+    formatValue: (value, asRaw = false) => {
+      const bn = formatLamports(value, decimals);
+      return asRaw ? bn.toString() : bn.toFormat();
+    },
+    parseValue: (value) => {
+      return new BN(formatLamports(value.replace(',', ''), -decimals).toString());
+    },
+    closestNumber: (value) => {
+      const bn = formatLamports(
+        formatLamports(value, decimals).integerValue(BigNumber.ROUND_CEIL), -decimals,
+      );
+      return new BN(bn.toString());
+    },
+  } as IMintFormatter;
+};
--- packages/governance/src/views/realm/buttons/depositGoverningTokensButton.tsx
+++ packages/governance/src/views/realm/buttons/depositGoverningTokensButton.tsx
@@ -1,17 +1,16 @@
-import { Button, Col, Modal, Row } from 'antd';
-import React, { useMemo, useState } from 'react';
+import { Button, Col, InputNumber, Modal, Row } from 'antd';
+import React, { useState } from 'react';
 import { ProgramAccount, Realm } from '@solana/spl-governance';
 import { LABELS } from '../../../constants';
 import { hooks } from '@oyster/common';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
-import {
-  depositGoverningTokens,
-} from '../../../actions/depositGoverningTokens';
+import { depositGoverningTokens } from '../../../actions/depositGoverningTokens';
 import { PublicKey } from '@solana/web3.js';
 import { useRpcContext } from '../../../hooks/useRpcContext';
 import BN from 'bn.js';
 import { useVoterWeightRecord } from '../../../hooks/apiHooks';
 import { useVestingProgramId } from '../../../hooks/useVestingProgramId';
+import { useMintFormatter } from '../../../hooks/useMintFormatter';
 
 const { useAccountByMint } = hooks;
 
@@ -28,42 +27,22 @@ export function DepositGoverningTokensButton ({
 }) {
   const rpcContext = useRpcContext();
   const governingTokenAccount = useAccountByMint(governingTokenMint);
+  const { closestNumber, formatValue, parseValue } = useMintFormatter(governingTokenMint) || {}
   const vestingProgramId = useVestingProgramId(realm);
   const voterWeightRecord = useVoterWeightRecord(realm);
 
-  const availableBalance = new BN(
-    (governingTokenAccount?.info.amount as BN) || 0
-  );
-  const [depositableAmount] = useState<BN>(
-    availableBalance.isZero()
-      ? new BN(100_000) : availableBalance.divn(2500)
-  );
+  const [isConfirmationVisible, setConfirmationVisible] = useState(false);
 
-  const depositConfirmation = useMemo(() => {
-    const amountPercentage = availableBalance.isZero()
-      ? 0 : (depositableAmount.muln(10000).div(availableBalance).toNumber() /
-        100);
+  const availableBalance = new BN(governingTokenAccount?.info.amount as BN || 0);
 
-    return <div>
-      <p>{LABELS.DEPOSIT_TOKENS_QUESTION}</p>
-      <Row>
-        <Col flex={1}>{LABELS.WALLET_BALANCE}:</Col>
-        <Col flex={1} style={{ textAlign: 'right' }}>{availableBalance.toString()}</Col>
-      </Row>
-      <Row>
-        <Col flex={1}>
-          Amount to deposit:
-        </Col>
-        <Col flex={1} style={{ textAlign: 'right' }}>
-          {depositableAmount.toString()}
-          <br />
-          ( {amountPercentage > 0.001
-          ? amountPercentage.toFixed(2)
-          : '< 0.001'}% of total )
-        </Col>
-      </Row>
-    </div>;
-  }, [availableBalance, depositableAmount]);
+  const defaultDepositableAmount = availableBalance.isZero()
+      ? closestNumber(100_000) : closestNumber(availableBalance.divn(4));
+
+  const [depositableAmount, setDepositableAmount] = useState<BN>(defaultDepositableAmount);
+
+  const amountPercentage = availableBalance.isZero()
+    ? 0 : (depositableAmount.muln(10_000).div(availableBalance).toNumber() /
+      100);
 
   if (!realm) {
     return null;
@@ -71,35 +50,77 @@ export function DepositGoverningTokensButton ({
 
   const isVisible = !availableBalance.isZero();
 
-  return isVisible ? (
+  return isVisible ? <>
     <Button
       type="primary"
-      onClick={() => {
+      onClick={() => setConfirmationVisible(true)}
+      onAbort={() => {
         confirm({
           title: LABELS.DEPOSIT_TOKENS(tokenName),
           icon: <ExclamationCircleOutlined />,
           // Modal.confirm content not reactive, so we can't provide inputbox
-          content: depositConfirmation,
           okText: LABELS.DEPOSIT,
           cancelText: LABELS.CANCEL,
-          onOk: async() => {
-            if (governingTokenAccount && voterWeightRecord) {
-              await depositGoverningTokens(
-                rpcContext,
-                realm!.pubkey,
-                governingTokenAccount,
-                governingTokenMint!,
-                depositableAmount,
-                vestingProgramId,
-                voterWeightRecord!.voterWeight.pubkey,
-                voterWeightRecord!.maxVoterWeight.pubkey,
-              );
-            }
-          },
         });
       }}
     >
       {LABELS.DEPOSIT_TOKENS(tokenName)}
     </Button>
-  ) : null;
+    <Modal
+      visible={isConfirmationVisible}
+      title={LABELS.DEPOSIT_TOKENS(tokenName)}
+      destroyOnClose
+      cancelText={LABELS.CANCEL}
+      onCancel={() => {
+        setConfirmationVisible(false);
+        // reset initial value
+        setDepositableAmount(defaultDepositableAmount);
+      }}
+      okText={LABELS.DEPOSIT}
+      onOk={async () => {
+        if (governingTokenAccount && voterWeightRecord) {
+          try {
+            await depositGoverningTokens(
+              rpcContext,
+              realm!.pubkey,
+              governingTokenAccount,
+              governingTokenMint!,
+              depositableAmount,
+              vestingProgramId,
+              voterWeightRecord!.voterWeight.pubkey,
+              voterWeightRecord!.maxVoterWeight.pubkey,
+            );
+          } catch (e) {
+            // user rejected transaction, noop
+          }
+        }
+      }
+      }
+    >
+      <p>{LABELS.DEPOSIT_TOKENS_QUESTION}</p>
+      <Row>
+        <Col flex={1}>{LABELS.WALLET_BALANCE}:</Col>
+        <Col flex={1} style={{ textAlign: 'right' }}>{
+          formatValue(availableBalance)
+        }</Col>
+      </Row>
+      <Row>
+        <Col flex={1}>
+          Amount to deposit:
+        </Col>
+        <Col flex={1} style={{ textAlign: 'right' }}>
+          <InputNumber<string>
+            value={formatValue(depositableAmount, true)}
+            min={'0'}
+            max={formatValue(availableBalance, true)}
+            onChange={(v) => setDepositableAmount(parseValue(v.toString()))}
+          />
+          <br />
+          ( {amountPercentage > 0.001
+          ? amountPercentage.toFixed(2)
+          : '< 0.001'}% of total )
+        </Col>
+      </Row>
+    </Modal>
+  </> : null;
 }
